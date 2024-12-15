f93330e3037aa169afa0f63f6ea45f3eee4370da
Gerald max <166443104+13065994@users.noreply.github.com>
2024-12-15 11:07:27 +0100

    Update prefix.js

--- modules/commands/prefix.js
+++ modules/commands/prefix.js
@@ -16,8 +16,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("prefix")==0 || (event.body.indexOf("Prefix")==0 || (event.body.indexOf("Ano prefix")==0 || (event.body.indexOf("ano prefix")==0)))) {
     const moment = require("moment-timezone");
-    var gio = moment.tz("Asia/Manila").format("HH:mm:ss || D/MM/YYYY");
-      api.shareContact(`My Current System Prefix is > ${global.config.PREFIX} <\nType ${global.config.PREFIX}help to see all commands\nTime: ${gio}`, api.getCurrentUserID(), event.threadID);
+    var gio = moment.tz("Africa/Lagos").format("HH:mm:ss || D/MM/YYYY");
+      api.shareContact(`Hello, My Current System Prefix is: ${global.config.PREFIX}\nType ${global.config.PREFIX}help to see all commands\n\n\t${gio}`, api.getCurrentUserID(), event.threadID);
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
